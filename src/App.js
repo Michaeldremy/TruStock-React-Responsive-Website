bd8623c4ad855d0da7955176bca17d537c9fd7b0
Michaeldremy <michaeldremy@gmail.com>
2020-10-16 13:40:06 -0700

    Finished SideBarMenu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Router>
+        <SideBar />
         <NavBar />
       </Router>
     </div>
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from "react";
+import {
+  SideBarContainer,
+  CloseIcon,
+  Icon,
+  SideBarWrapper,
+  SideBarMenu,
+  SideBarLink,
+  SideBtnWrap,
+  SideBarRoute,
+} from "./SideBarElements";
 
 const SideBar = () => {
   return (
     <>
-    <SideBarContainer>
-      <Icon>
-        <CloseIcon />
-      </Icon>
-    </SideBarContainer>
+      <SideBarContainer>
+        <Icon>
+          <CloseIcon />
+        </Icon>
+        <SideBarWrapper>
+          <SideBarMenu>
+            <SideBarLink to="about">About</SideBarLink>
+            <SideBarLink to="discover">Discover</SideBarLink>
+            <SideBarLink to="services">Services</SideBarLink>
+            <SideBarLink to="signup">Sign Up</SideBarLink>
+          </SideBarMenu>
+          <SideBtnWrap>
+            <SideBarRoute to="/signin">Sign In</SideBarRoute>
+          </SideBtnWrap>
+        </SideBarWrapper>
+      </SideBarContainer>
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
