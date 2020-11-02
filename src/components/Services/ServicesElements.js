1481932044eebbf0e664da763838ffd8de9b0206
Michaeldremy <michaeldremy@gmail.com>
2020-11-02 13:10:39 -0800

    Added styling for ServicesIcon and ServicesH1

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -53,3 +53,19 @@ export const ServicesCard = styled.div`
     cursor: pointer;
   }
 `
+
+export const ServicesIcon = styled.img`
+  height: 160px;
+  width: 160px;
+  margin-bottom: 10px;
+`
+
+export const ServicesH1 = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-bottom: 64px;
+
+  @media screen and (max-width: 480px){
+    font-size: 2rem;
+  }
+`
