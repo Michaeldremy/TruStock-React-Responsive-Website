c00949fe37edc39b9c96c465f2ceaf45d04204ac
Michaeldremy <michaeldremy@gmail.com>
2020-11-06 13:15:22 -0800

    Addded react scroll for the logo on the main nav and footer page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { animateScroll as scroll } from "react-scroll";
 import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa';
 import {FooterContainer,  FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink} from './FooterElements';
 
 const Footer = () => {
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <FooterContainer>
       <FooterWrap>
@@ -44,7 +50,7 @@ const Footer = () => {
         </FooterLinksContainer>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to="/">
+            <SocialLogo to="/" onClick={toggleHome}>
               TruStock
             </SocialLogo>
             <WebsiteRights>TruStock © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { animateScroll as scroll } from "react-scroll";
 import {
   Nav,
   NavBarContainer,
@@ -21,40 +23,47 @@ const NavBar = ({ toggle }) => {
     } else {
       setScrollNav(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', changeNav);
-  }, [])
+    window.addEventListener("scroll", changeNav);
+  }, []);
 
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
 
   return (
     <>
-      <Nav scrollNav={scrollNav}>
-        <NavBarContainer>
-          <NavLogo to="/">TruStock</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBars />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to="about">About</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="discover">Discover</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="services">Services</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="signup">Sign Up</NavLinks>
-            </NavItem>
-          </NavMenu>
-          <NavBtn>
-            <NavBtnLink to="/signin">Sign In</NavBtnLink>
-          </NavBtn>
-        </NavBarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavBarContainer>
+            <NavLogo to="/" onClick={toggleHome}>
+              TruStock
+            </NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="about">About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="discover">Discover</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="signup">Sign Up</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavBarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
